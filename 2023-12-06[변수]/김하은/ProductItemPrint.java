4b7a7cbad35fbf58b2fa55f45a2881f9091e0829
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-06 18:23:55 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/김하은/ProductItemPrint.java
@@ -0,0 +1,25 @@
+
+public class ProductItemPrint {
+
+	public static void main(String[] args) {
+		
+		String name;
+		String price;
+		String delivery;
+		int review;
+		
+		name="마론디 레더 패딩";
+		price="9,2000원";
+		delivery="12월 11일 예약 발송";
+		review=9;
+		
+		System.out.println("---------------------------");
+		System.out.println("이름 : "+name);
+		System.out.println("가격 : "+price);
+		System.out.println("배송 : "+delivery);
+		System.out.println("후기 : "+review);	
+		System.out.println("---------------------------");
+
+	}
+
+}
