68feb2fbfc382f377e8efe9fc8242f48131f03fb
김수환 <152938625+SOOHWAN2@users.noreply.github.com>
2023-12-06 18:07:44 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/김수환/BoardItemPrint.java
@@ -0,0 +1,21 @@
+
+public class BoardItemPrint {
+
+	public static void main(String[] args) {
+		
+		int num=1;
+		
+		int view=1312;
+		String date="2023-12-06";
+		String title="사랑니 발치 너무 아파요";
+		String name="김수환";
+		
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println("순번 |                제목              | 등록자명 |  등록일  | 조회수 ");
+		System.out.println("-----------------------------------------------------------------------");
+		System.out.println(num+"\t"+title+"\t\t"+name+"\t    "+date+"\t "+view);
+		System.out.println("-----------------------------------------------------------------------");
+		
+	}
+
+}
