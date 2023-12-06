a61b59a60bd2f9ba1695add44ffb123deb0f11da
sdf45654fds <152965357+sdf45654fds@users.noreply.github.com>
2023-12-06 18:05:47 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/이재훈/DuoMemberInfoPrint.java
@@ -0,0 +1,26 @@
+
+public class DuoMemberInfoPrint {
+
+	public static void main(String[] args) {
+		int a=236514, c1=201212, c2=3495039;
+		char b1='남',b2='\u0030',b3='\u0041';
+		String str1="홍길동",str2="상(중)";
+		boolean e=true;
+		double f=178.3;
+		
+		
+		System.out.println("******************************");
+		System.out.println("회원번호 : "+a);
+		System.out.println("이    름 : "+str1);
+		System.out.println("주민번호 : "+c1+"-"+c2);
+		System.out.println("성    별 : "+b1);
+		System.out.println("결혼횟수 : "+b2);
+		System.out.println("흡연여부 : "+e);
+		System.out.println("키       : "+f);
+		System.out.println("학     점: "+b3);
+		System.out.println("용     모: "+str2);
+		System.out.println("******************************");
+
+	}
+
+}
