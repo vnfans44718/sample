fb5d9ac6535de37a68768fd6946cbef397f0a8f7
지민수 <152963671+sjnltt3756@users.noreply.github.com>
2023-12-06 20:15:51 +0900

    Add files via upload

--- 2023-12-06[변수]/지민수/Quiz.java
+++ 2023-12-06[변수]/지민수/Quiz.java
@@ -2,41 +2,31 @@
 public class Quiz {
 
 	public static void main(String[] args) {
-		
 		/*
 		 * Quiz: 윤년여부판단
 		 *    - 4의배수(4로나누어떨어지는수)이면서 100의배수가아닌수(100로나누어떨어지는않는수) 이거나
 		 *    - 400의배수(400로나누어떨어지는수)
 		 */
-		
 		int year = 2023;
-		boolean isLeapYear = ((year % 4 == 0) && (year%100!=0) || (year%400==0));
-		
-		/*
-		 * if ((year % 4 == 0) && (year%100!=0) || (year%400==0)) {
-			isLeapYear = true;
-		}
-		 */
+		boolean isLeapYear = (year%4 == 0 && year%100 != 0) || (year%400 == 0);
 		
 		System.out.println(year+ "의 윤년여부:"+isLeapYear);
-		
 		
 		/*
 		 * Quiz: 한글여부판단
 		 *  
 		 */
-		char c = '비';
-		boolean isHangul = (c >= '가' && c <='힣') || (c >= 'ㄱ' && c <= 'ㅎ');
+		char c = 'ㅢ';
+		boolean isHangul = (c >='가' && c <= '힣') || (c >='ㄱ' && c <= 'ㅎ') || (c >='ㅏ' && c <= 'ㅣ');
 		System.out.println("한글여부:"+isHangul);
 		
 		/*
 		 * Quiz:아이디의첫글자는 영문대문자이거나 소문자여야한다.
 		 */
-		char idFirstLetter = 'a';
-		boolean isValidIdFirstLetter = (idFirstLetter >= 'A' && idFirstLetter <='Z') || (idFirstLetter >= 'a' && idFirstLetter <='z');
+		char idFirstLetter = 'u';
+		boolean isValidIdFirstLetter= (idFirstLetter >= 'A' && idFirstLetter <= 'Z') || (idFirstLetter >= 'a' && idFirstLetter <= 'z');
 		System.out.println(idFirstLetter+" 의 적합성여부:"+isValidIdFirstLetter);
-	
-		
+
 	}
 
 }
