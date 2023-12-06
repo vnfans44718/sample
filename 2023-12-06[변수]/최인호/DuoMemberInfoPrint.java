b44fd92082a25c1bf46216b6340aacc99be852f1
최인호 <100062988+backrupt@users.noreply.github.com>
2023-12-06 16:50:58 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/최인호/DuoMemberInfoPrint.java
@@ -0,0 +1,29 @@
+
+public class DuoMemberInfoPrint {
+	public static void main(String[] args) {
+		System.out.println("*************************");
+		
+		int id = 236514;
+		String name = "최인호"; 
+		String socialSecurityNumber = "990525-1234567";
+		String sex = "남";
+		int numberOfMarriages = 0;
+		boolean smoking = false;
+		double height = 187.8;
+		char grade = 'A';
+		String face = "상(중)";
+		
+		System.out.println("회원번호 : "+id);
+		System.out.println("이  름 : "+name);
+		System.out.println("주민번호 : "+socialSecurityNumber);
+		System.out.println("성  별 : "+sex);
+		System.out.println("결혼횟수 : "+numberOfMarriages);
+		System.out.println("흡연여부 : "+smoking);
+		System.out.println("키 : "+height);
+		System.out.println("학  점 : "+grade);
+		System.out.println("용  모 : "+face);
+		
+		System.out.println("*************************");
+	}
+
+}
