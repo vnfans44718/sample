bee2f0b6622975ac1ffee97d7258c41c4bb892c2
원주하 <152963740+vnfans44718@users.noreply.github.com>
2023-12-06 17:26:13 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/원주하/DuoMemberInfoPrint.java
@@ -0,0 +1,44 @@
+
+public class DuoMemberInfoPrint {
+
+	public static void main(String[] args) {
+		int 회원번호,결혼횟수;
+		double 키;
+		char 성별,학점,용모;
+		String 이름,주민번호;
+		boolean 흡연여부;
+		
+		
+		회원번호=236514;
+		이름= "원주하";
+		주민번호="123456-78901";
+	    성별='여';
+		결혼횟수=0;
+		흡연여부=false;
+		키=153.0;
+		학점='A';
+		용모='중';
+		
+		System.out.println("********************************************************************");	
+	 System.out.println("회원번호 :"+회원번호);
+	 System.out.println("이름 :"+이름);
+	 System.out.println("주민번호 :"+주민번호);
+	 System.out.println("성별 :"+성별);
+	 System.out.println("결혼횟수 :"+결혼횟수);
+	 System.out.println("흡연여부 :"+흡연여부);
+	 System.out.println("키 :"+키);
+	 System.out.println("키 :"+학점);
+	 System.out.println("키 :"+용모);
+	 System.out.println("********************************************************************");
+	 System.out.println();
+	 System.out.println("********************************************************************");
+	 System.out.println("회원번호  이름   주민번호   성별  결혼횟수   흡연여부  키  학점  용모");
+	 System.out.println("********************************************************************");
+	 System.out.println(회원번호+"  "+ 이름 +"  "+ 주민번호 +"  "+ 성별 +"    "+ 결혼횟수 +"         "+ 흡연여부 +"   "+ 키 +"  "+ 학점 +"   "+ 용모);
+	 
+	 
+	 
+
+	}
+
+}
