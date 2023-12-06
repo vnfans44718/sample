0e3bc31cf91f963a1248deeedeca33360cc19d4b
academyitwill <academyitwill@gmail.com>
2023-12-06 14:30:07 +0900

    no message

---
+++ 2023-12-06[변수]/김경호/DuoMemberInfoPrint.java
@@ -0,0 +1,21 @@
+public class DuoMemberInfoPrint{
+	public static void main(String[] args) {
+		/*
+		 * 1.변수선언
+		 */
+		
+		
+		/*
+		 * 2.변수에 데이타(리터럴)대입
+		 */
+		
+		/*
+		 * 3.변수 데이타 출력
+		 */
+		System.out.println("*********************************");
+		
+		System.out.println("*********************************");
+		
+		
+	}
+}
