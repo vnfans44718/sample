82cbd7ec10fec3a77241818fa4ab9b2c4f7ef553
최기창 <152938540+dpzh13@users.noreply.github.com>
2023-12-06 18:05:57 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/최기창/DuoMemberInfoprint.java
@@ -0,0 +1,49 @@
+
+public class DuoMemberInfoprint {
+
+	public static void main(String[] args) {
+		
+		    int usernumber;
+		    String name;
+		    String number;
+		    char gender;
+		    int marry;
+		    boolean smoking;
+		    double height;
+		    char grade;
+		    String face;
+			
+			usernumber = 236514;
+			name = "홍길동";
+			number = "201212-3495039";
+			gender = '남';
+			marry = 0;
+			smoking = true;
+			height = 178.3;
+			grade = 'A';
+			face = "상(중)";
+			
+			System.out.println("***************************");
+			System.out.println("회원번호 : "+usernumber);
+			System.out.println("이    름 : "+name);
+			System.out.println("주민번호 : "+number);
+			System.out.println("성    별 : "+gender);
+			System.out.println("결혼횟수 : "+marry);
+			System.out.println("흡연여부 : "+smoking);
+			System.out.println("키       : "+height);
+			System.out.println("학    점 : "+grade);
+			System.out.println("용    모 : "+face);
+			System.out.println("***************************");
+		
+		
+		
+		
+		
+		
+		
+		
+				
+		
+	}
+
+}
