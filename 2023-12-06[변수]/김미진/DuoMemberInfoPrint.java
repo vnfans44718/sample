4761ebbd077b5502311d2c78242bf94da65530f1
김미진 <149574990+slwla@users.noreply.github.com>
2023-12-06 17:41:27 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/김미진/DuoMemberInfoPrint.java
@@ -0,0 +1,46 @@
+
+public class DuoMemberInfoPrint {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.변수선언
+		 */
+		int num1;
+		String name;
+		String num2;
+		char gender;
+		int num3;
+		boolean smoking;
+		double height;
+		char grade;
+		String face;
+		/*
+		 * 2.변수에 데이타(리터럴)대입
+		 */
+		num1 = 236514;
+		name = "홍길동";
+		num2 = "201212-3495039";
+		gender = '남';
+		num3 = 0;
+		smoking =  true;
+		height = 178.3;
+		grade = 'A';
+		face = "상(중)";
+		/*
+		 * 3.변수 데이타 출력
+		 */
+		System.out.println("*********************************");
+		System.out.println("회원번호: "+num1);
+		System.out.println("이    름: " +name);
+		System.out.println("주민번호: "+num2);
+		System.out.println("성    별: "+gender);
+		System.out.println("결혼횟수: "+num3);
+		System.out.println("흡연여부: "+smoking);
+		System.out.println("키      : "+height);
+		System.out.println("학    점: " +grade);
+		System.out.println("용    모: "+face);
+		System.out.println("*********************************");
+
+	}
+
+}
