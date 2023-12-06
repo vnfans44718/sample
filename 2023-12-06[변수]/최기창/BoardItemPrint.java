54f6e6d62d0bac9d63829961665d7ea7948be250
최기창 <152938540+dpzh13@users.noreply.github.com>
2023-12-06 20:05:18 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/최기창/BoardItemPrint.java
@@ -0,0 +1,24 @@
+
+public class BoardItemPrint {
+
+	public static void main(String[] args) {
+		
+		int num1 = 123;
+		String tit = "관리자 삭제 게시물";
+		String name = "최기창";
+		String date = "2023.12.06";
+		int num2 = 321;
+	
+		
+		System.out.println("-----------------------------------------------------------------------------------");
+		System.out.println("순번 \t\t\t 제목 \t\t작성자 \t\t 작성일 \t\t조회수");
+		System.out.println("-----------------------------------------------------------------------------------");
+		System.out.println(num1+ "\t\t"+tit+"\t"+name+"\t\t"+date+"\t"+num2+ "\t\t");
+		System.out.println("-----------------------------------------------------------------------------------");
+		
+		
+		
+		
+	}
+
+}
