2af38bf2fd6eab8f5aac5b592b5bc0620a785b0c
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-06 16:31:18 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/김하은/VariableDeclare1.java
@@ -0,0 +1,33 @@
+
+public class VariableDeclare1 {
+
+	public static void main(String[] args) {
+		int a;//변수선언
+		a = 10;//변수초기화
+		System.out.println(a);//a변수값출력
+		a = 50;//변수값변경
+		System.out.println(a);//a변수값출력
+		
+		int b;
+		int c;
+		int d,e;
+		
+		b = 12;
+		c = 34;
+		d = 67;
+		e = 90;
+		
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		
+	}
+
+}
+
+
+
+
+	
