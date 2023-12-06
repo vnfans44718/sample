9e519f80363e774e375098aa5d507c5760e3a222
김미진 <149574990+slwla@users.noreply.github.com>
2023-12-06 18:22:23 +0900

    Add files via upload

---
+++ 2023-12-06[변수]/김미진/ProductItemPrint.java
@@ -0,0 +1,26 @@
+
+public class ProductItemPrint {
+
+	public static void main(String[] args) {
+		
+		String img;
+		String name;
+		String price;
+		String company;
+
+		
+		img = "이미지";
+		name = "크리스마스 대형트리 180cm (장식포함)";
+		price = "132,750원";
+		company = "(주)메리지니마스";
+		
+		System.out.println("*********************************");
+		System.out.println(img);
+		System.out.println(name);
+		System.out.println(price);
+		System.out.println(company);
+		System.out.println("*********************************");
+		
+	}
+	
+}
