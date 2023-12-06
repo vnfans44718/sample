b34dc8e6738b918ea5982ae8bf2d6728ff82cd92
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-06 17:46:44 +0900

    Add files via upload

--- 2023-12-06[변수]/김하은/DuoMemberInfoPrint.java
+++ 2023-12-06[변수]/김하은/DuoMemberInfoPrint.java
@@ -31,7 +31,6 @@ public class DuoMemberInfoPrint {
 		System.out.println("***************************************************************************************");
 		System.out.println(duoInfo);
 		
-		
 	}
 	
 }
