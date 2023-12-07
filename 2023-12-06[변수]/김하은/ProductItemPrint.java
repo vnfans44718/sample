c90288a94bfe5c90cf3e041528c5d5c2a6d8608a
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-07 10:05:53 +0900

    Add files via upload

--- 2023-12-06[변수]/김하은/ProductItemPrint.java
+++ 2023-12-06[변수]/김하은/ProductItemPrint.java
@@ -4,20 +4,20 @@ public class ProductItemPrint {
 	public static void main(String[] args) {
 		
 		String name;
-		String price;
+		int price;
 		String delivery;
 		int review;
 		
 		name="마론디 레더 패딩";
-		price="9,2000원";
+		price=92000;
 		delivery="12월 11일 예약 발송";
 		review=9;
 		
 		System.out.println("---------------------------");
 		System.out.println("이름 : "+name);
-		System.out.println("가격 : "+price);
+		System.out.println("가격 : "+price+"원");
 		System.out.println("배송 : "+delivery);
-		System.out.println("후기 : "+review);	
+		System.out.println("후기 : "+review+"개");	
 		System.out.println("---------------------------");
 
 	}
