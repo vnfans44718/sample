0e20a99d8ebfe5ac91413d5e9c90c7ae4eb2953e
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-07 10:04:14 +0900

    Add files via upload

--- 2023-12-06[변수]/김하은/CartItemPrint.java
+++ 2023-12-06[변수]/김하은/CartItemPrint.java
@@ -6,24 +6,24 @@ public class CartItemPrint {
 		String name;
 		String option;
 		int pcs;
-		String productFee;
-		String deliveryFee;
-		String totalFee;
+		int productFee;
+		int deliveryFee;
+		int totalFee;
 		
 		name="마론디 레더 패딩";
 		option="블랙";
 		pcs=1;
-		productFee="92,000원";
-		deliveryFee="0원";
-		totalFee="92,000원";
+		productFee=92000;
+		deliveryFee=0;
+		totalFee=92000;
 		
 		System.out.println("---------------------------");
 		System.out.println("이름 : "+name);
 		System.out.println("옵션 : "+option);
 		System.out.println("수량 : "+pcs);
-		System.out.println("상품 금액 : "+productFee);	
-		System.out.println("배송비 : "+deliveryFee);	
-		System.out.println("총 주문금액 : "+totalFee);	
+		System.out.println("상품 금액 : "+productFee+"원");	
+		System.out.println("배송비 : "+deliveryFee+"원");	
+		System.out.println("총 주문금액 : "+totalFee+"원");	
 		System.out.println("---------------------------");
 		
 	}
