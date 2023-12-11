ba3209105d9d63d33c2712452d2bf6c510315250
송선경 <151636445+jessdev512@users.noreply.github.com>
2023-12-11 18:19:10 +0900

    Add files via upload

---
+++ 2023-12-11[멤버필드]/송선경/Student.java
@@ -0,0 +1,38 @@
+package com.itwill00.variable;
+
+public class Student {
+
+		//멤버필드 선언 
+		
+		// 학생1
+		int no1; 
+		String name1;
+		int kor1;
+		int eng1;
+		int math1;
+		double tot1;
+		double avg1;
+		char grade1;
+		int rank1;
+		
+		// 학생2
+		int no2; 
+		String name2;
+		int kor2;
+		int eng2;
+		int math2;
+		double tot2;
+		double avg2;
+		char grade2;
+		int rank2;
+		
+		
+		/*
+		 * 멤버필드 선언할 때 
+		 * main 메소드 위에 (밖에) 선언하는 것 잊지 말기!
+		 * 메소드 안쪽에 선언하면 오류 남 
+		 */
+		
+	}
+
+
