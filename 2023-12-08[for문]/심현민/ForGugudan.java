8fe757188fed1243baedfd20bca44bd62f631d17
심현민 <152938480+Shimhyunmin@users.noreply.github.com>
2023-12-11 17:21:12 +0900

    Add files via upload

---
+++ 2023-12-08[for문]/심현민/ForGugudan.java
@@ -0,0 +1,15 @@
+
+public class ForGugudan {
+
+	public static void main(String[] args) {
+		
+		for (int j = 1; j <= 9 ; j++) {
+			for(int i = 2; i <= 9; i++) {
+				System.out.printf("%d * %d = %2d ",i,j,i*j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
