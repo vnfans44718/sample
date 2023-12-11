86dc6c04fe7172dde69b51079805028815d327ba
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-11 17:22:08 +0900

    Add files via upload

--- 2023-12-11[멤버필드]/김하은/Student.java
+++ 2023-12-11[멤버필드]/김하은/Student.java
@@ -3,7 +3,6 @@ package com.itwill01.field;
  * 성적처리를위해 필요한 학생객체를 만들기위한클래스정의(틀,타입)
  */
 public class Student {
-	
 	/*
 	 * 1.멤버필드(변수)선언:속성
 	 *   -번호,이름,국어,영어,수학,총점,평균,평점(A,B,C,D,F),석차
@@ -18,5 +17,4 @@ public class Student {
 	double avg;
 	char score;
 	int rank;
-	
 }
