03ea11b2d0cc34bcfa0a09ae9811ec02cb56df33
이재훈 <152965357+sdf45654fds@users.noreply.github.com>
2023-12-11 18:07:49 +0900

    Add files via upload

---
+++ 2023-12-11[멤버필드]/이재훈/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+	
+	int no1; 	  //학번
+	String name1; //이름
+	int kor1;	  //국어
+	int eng1;	  //영어
+	int mth1;    //수학
+	int tot1;    //총점
+	double avg1;   //평균
+	char grade1;		  //평점
+	
+	int no2; 	  //학번
+	String name2; //이름
+	int kor2;     //국어
+	int eng2;     //영어
+	int mth2;     //수학
+	int tot2;     //총점
+	double avg2;  //평균
+	char grade2;		  //평점
+	
+}
