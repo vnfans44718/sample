c517fe8755e1fcc041d4d5b2501146cfd066122f
김민섭 <152938799+Kimminsub1@users.noreply.github.com>
2023-12-12 18:09:24 +0900

    Create Calculator.java

---
+++ 2023-12-12[멤버메쏘드]/김민섭/Calculator.java
@@ -0,0 +1,25 @@
+package com.itwill01.MemberMethod;
+
+public class Calculator {
+
+	public static int add(int x, int y) {
+		int sum1 = x + y;
+		return sum1;
+	}
+
+	public static int sub(int x, int y) {
+		int sum1 = x - y;
+		return sum1;
+	}
+
+	public static int mul(int x, int y) {
+		int sum1 = x * y;
+		return sum1;
+	}
+
+	public static int div(int x, int y) {
+		int sum1 = x / y;
+		return sum1;
+	}
+
+}
