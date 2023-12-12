0ac86fb4997a32a649cfa293fecbb2b399a61675
심현민 <152938480+Shimhyunmin@users.noreply.github.com>
2023-12-12 23:09:22 +0900

    Update Calculator.java

--- 2023-12-12[멤버메쏘드]/심현민/Calculator.java
+++ 2023-12-12[멤버메쏘드]/심현민/Calculator.java
@@ -1,22 +1,20 @@
 package com.itwill01.method;
 
 public class Calculator {
-
-	int add(int x, int y) {
+	public static int add(int x, int y) {
 		int result = x + y;
 		return result;
 	} 
-	int sub(int x, int y) {
+	public static int sub(int x, int y) {
 		int result = x - y;
 		return result;
 	} 
-	 double div(int x, int y) {
-		 double result = (double) x / (double) y;
+	public static int div(int x, int y) {
+		 int result = x / y;
 		 return result;
 	} 
-	int mul(int x, int y) {
+	public static int mul(int x, int y) {
 		int result = x * y;
 		return result;
 	} 
-
 }
