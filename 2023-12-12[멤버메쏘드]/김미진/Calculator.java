88149cc528ce0cb8e157b6e4012db888cde94c88
김미진 <149574990+slwla@users.noreply.github.com>
2023-12-12 17:47:39 +0900

    Add files via upload

---
+++ 2023-12-12[멤버메쏘드]/김미진/Calculator.java
@@ -0,0 +1,39 @@
+package com.itwill01.method;
+
+/*
+ * 4개의 멤버메쏘드를 가짐
+ */
+public class Calculator {
+	/*
+	 * - add( 두개의 정수를 매개변수로 받아서 더한 결과값을 반환)
+	 * - sub( 두개의 정수를 매개변수로 받아서 뺀   결과값을 반환)
+	 * - div( 두개의 정수를 매개변수로 받아서 나눈 결과값을 반환)
+	 * - mul( 두개의 정수를 매개변수로 받아서 곱한 결과값을 반환)
+	 */
+
+	// add
+	public static int add(int a, int b) {
+		int add = a + b;
+		return add;
+	}
+		
+	// sub
+	public static int sub(int a, int b) {
+		int sub = a - b;
+		return sub;
+	}
+			
+	// div
+	public static int div(int a, int b) {
+		int div = a / b;
+		return div;
+	}
+				
+	// mul
+	public static int mul(int a, int b) {
+		int mul = a * b;
+		return mul;
+	}
+	
+	
+}
