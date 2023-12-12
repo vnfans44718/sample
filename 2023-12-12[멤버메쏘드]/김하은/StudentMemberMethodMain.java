a7949c4f0ada280541d19fd4960b381f73dd4a89
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-12 17:53:52 +0900

    Add files via upload

--- 2023-12-12[멤버메쏘드]/김하은/StudentMemberMethodMain.java
+++ 2023-12-12[멤버메쏘드]/김하은/StudentMemberMethodMain.java
@@ -12,9 +12,9 @@ public class StudentMemberMethodMain {
 		/*
 		 학생객체기본데이타(번호, 이름, 국어, 영어, 수학) 입력메쏘드호출(3명)
 		 */
-		student1.dataInput(1, "남다연", 97, 96, 80);
-		student2.dataInput(2, "최예나", 80, 82, 98);
-		student3.dataInput(3, "우지희", 88, 78, 64);
+		student1.basicData(1, "남다연", 97, 96, 80);
+		student2.basicData(2, "최예나", 80, 82, 98);
+		student3.basicData(3, "우지희", 88, 78, 64);
 			
 		/*
 		 학생총점계산 메쏘드 호출(3명)
@@ -37,9 +37,9 @@ public class StudentMemberMethodMain {
 		/*
 		 * 학생데이타 출력메쏘드 호출(3명)
 		 */
-		student1.headerOutput();
-		student1.dataOutput();
-		student2.dataOutput();
-		student3.dataOutput();
+		student1.headerPrint();
+		student1.dataPrint();
+		student2.dataPrint();
+		student3.dataPrint();
 	}
 }
