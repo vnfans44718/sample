eafb2efe3b8b8fd917c0ae4177327b02b2701024
김민섭 <152938799+Kimminsub1@users.noreply.github.com>
2023-12-13 20:13:12 +0900

    Create HealthMain.java

---
+++ 2023-12-12[멤버메쏘드]/김민섭/HealthMain.java
@@ -0,0 +1,14 @@
+package com.itwill01.Method;
+
+public class HealthMain {
+
+	public static void main(String[] args) {
+		Health teach1 = new Health();
+		Health teach2 = new Health();
+
+		teach1.teacherInfo(180, 78, "남성", "Tomas", 37, "PT담당");
+		teach2.teacherInfo(170, 50, "여성", "Alice", 26, "요가담당");
+
+	}
+
+}
