7f39b4b564f5af0e812877e4278b2c71ca4d8fa1
김진효 <152963643+ppnyoong9@users.noreply.github.com>
2023-12-13 20:03:35 +0900

    Add files via upload

---
+++ 2023-12-13[선택과제]/객체모델링/김진효/Product.java
@@ -0,0 +1,52 @@
+package com.itwii.shop;
+
+public class Product {
+	int no;
+	String title;
+	int price;
+	String info;
+
+	// 상품 객체에 멤버변수를 대입하기 위한 메소드
+	
+	// 상품번호, 상품이름, 상품가격, 상품정보
+
+	public void product(int no, String title, int price, String info) {
+		this.no = no;
+		this.title = title;
+		this.price = price;
+		this.info = info;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+
+}
