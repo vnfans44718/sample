af7b7255dfecdd448c0ba9ffc0d8e44e123c5c12
최인호 <100062988+backrupt@users.noreply.github.com>
2023-12-18 18:09:01 +0900

    Add files via upload

---
+++ 2023-12-18[Car배열]/최인호/Car.java
@@ -0,0 +1,78 @@
+package com_12_17_student_array;
+
+public class Car {
+	private String no;// 차량번호
+	private int inTime;// 입차시간
+	private int outTime;// 출차시간
+	private int fee;// 주차요금
+
+	public Car() {
+
+	}
+
+	public Car(String no, int inTime) {
+		this.no = no;
+		this.inTime = inTime;
+	}
+
+	public Car(String no, int inTime, int outTime, int fee) {
+		this.no = no;
+		this.inTime = inTime;
+		this.outTime = outTime;
+		this.fee = fee;
+	}
+
+	public void setIpChaData(String no, int inTime) {
+
+		this.setNo(no);
+		this.setInTime(inTime);
+	}
+
+	public void calculateFee() {
+		this.fee = (this.outTime - this.inTime) * 1000;
+	}
+
+	public static void headerPrint() {
+		System.out.println("-----------------------------------");
+		System.out.printf("%s %s %s %s%n", "차량번호", "입차시간", "출차시간", "주차요금");
+		System.out.println("-----------------------------------");
+	}
+
+	public void print() {
+		System.out.printf("%7s %7d %9d %8d%n", this.no, this.inTime, this.outTime, this.fee);
+		return;
+	}
+
+	public void setOutTime(int outTime) {
+		this.outTime = outTime;
+	}
+
+	public String getNo() {
+		return no;
+	}
+
+	public void setNo(String no) {
+		this.no = no;
+	}
+
+	public int getInTime() {
+		return inTime;
+	}
+
+	public void setInTime(int inTime) {
+		this.inTime = inTime;
+	}
+
+	public int getFee() {
+		return fee;
+	}
+
+	public void setFee(int fee) {
+		this.fee = fee;
+	}
+
+	public int getOutTime() {
+		return outTime;
+	}
+
+}
---
+++ 2023-12-18[Car배열]/최인호/CarArrayMain.java
@@ -0,0 +1,76 @@
+package com_12_17_student_array;
+
+public class CarArrayMain {
+
+	public static void main(String[] args) {
+
+		Car[] carArray = { null, null, new Car("5654", 8), null, null, new Car("3422", 12), null, null, null, null,
+				null, null, null, null, new Car("7789", 11), new Car("1120", 10), null, null, null, null, null,
+				new Car("2389", 9), null, null, null, null, null, null, null, new Car("5555", 6) };
+
+		System.out.println("1.전체 차량출력");
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null) {
+				carArray[i].print();
+			}
+		}
+		System.out.println("2.전체주차구획수");
+		System.out.println("전체 주차 구획수 >> " + carArray.length);
+
+		System.out.println("3.남은주차구획수");
+		int num = 0;
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] == null) {
+				num++;
+			}
+		}
+		System.out.println("남은 주차 구획수 >> " + num);
+
+		System.out.println("4.입차");
+		Car car = new Car("7777", 9);
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] == null) {
+				carArray[i] = car;
+				break;
+			}
+		}
+
+		System.out.println("5.차량번호 7789번  차한대 정보출력");
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo() == "7789") {
+				carArray[i].print();
+			}
+		}
+		System.out.println("6.입차시간 10시이후 차량여러대 찾아서 정보출력");
+
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getInTime() >= 10) {
+				carArray[i].print();
+			}
+		}
+		System.out.println("7.2389번차량 12시 출차");
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo() == "2389") {
+				carArray[i].setOutTime(12);
+				carArray[i].calculateFee();
+			}
+		}
+
+		System.out.println("99. 2389번차량 출차후전체 차량출력");
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null && carArray[i].getNo() == "2389") {
+				carArray[i] = null;
+				break;
+			}
+		}
+		Car.headerPrint();
+		for (int i = 0; i < carArray.length; i++) {
+			if (carArray[i] != null) {
+				carArray[i].print();
+			}
+		}
+
+	}
+
+}
