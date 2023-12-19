1cbb4a06c68f63ad40723c555f1f4ecc0d796a9e
송선경 <151636445+jessdev512@users.noreply.github.com>
2023-12-19 22:08:38 +0900

    Add files via upload

---
+++ 2023-12-19[선택과제][1.클래스,객체,실행흐름그림]/송선경/A.java
@@ -0,0 +1,11 @@
+package reviewJava;
+
+public class A {
+
+		//멤버변수 2개 선언 
+		int no;
+		String color;
+		
+	
+
+}
---
+++ 2023-12-19[선택과제][1.클래스,객체,실행흐름그림]/송선경/AMain.java
@@ -0,0 +1,21 @@
+package reviewJava;
+
+public class AMain {
+
+	public static void main(String[] args) {
+
+	//A객체 생성 
+	A a1 = new A();
+	
+	//A객체멤버변수에 값대입 
+	a1.no=1;
+	a1.color="red";
+	
+	//A객체멤버변수의 값 출력 
+	System.out.println("a1 번호: "+a1.no);
+	System.out.println("a1 컬러: "+a1.color);
+
+		
+	}
+
+}
