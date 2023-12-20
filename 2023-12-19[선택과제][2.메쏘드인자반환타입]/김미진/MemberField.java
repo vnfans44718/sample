815fb341438972aca54d823bb74c586f6b5400c1
김미진 <149574990+slwla@users.noreply.github.com>
2023-12-20 18:07:22 +0900

    Add files via upload

---
+++ 2023-12-19[선택과제][2.메쏘드인자반환타입]/김미진/MemberField.java
@@ -0,0 +1,18 @@
+package com.itwill99.review;
+/*
+ * 1.객체를 생성할 클래스
+ * 2.데이타타입
+ */
+public class MemberField {
+	
+	/*
+	1. 멤버변수4개생성
+	*/
+	
+	String mf1;
+	String mf2;
+	String mf3;
+	int mf4;
+	
+	
+}
