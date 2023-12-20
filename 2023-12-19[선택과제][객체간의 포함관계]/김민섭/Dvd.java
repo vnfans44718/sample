6d7ec982c904923f16edb5759ce0b1e775bdddd5
김민섭 <152938799+Kimminsub1@users.noreply.github.com>
2023-12-20 17:53:03 +0900

    Create Dvd.java

---
+++ 2023-12-19[선택과제][객체간의 포함관계]/김민섭/Dvd.java
@@ -0,0 +1,62 @@
+package com.포함;
+
+public class Dvd {
+
+	/*
+	 * - 캡슐화하세요
+	 * - 생성자를 정의하세요
+	 * 
+	 * <<속성>>
+	 *  번호
+		타이틀
+		쟝르
+	 * 
+	 * <<기능>>
+	 *   Dvd정보출력
+	 *   
+	 */
+
+	private int no;
+	private String title;
+	private String genre;
+
+	public Dvd() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Dvd(int no, String title, String genre) {
+		super();
+		this.no = no;
+		this.title = title;
+		this.genre = genre;
+	}
+
+	public void print() {
+		System.out.printf("DVD번호 : %d \t 제목 : %s \t 장르 : %s\n", no, title, genre);
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+}
