e6e803a0f1e38e1a1a8aa60e5d7bec259bb6ad87
송선경 <151636445+jessdev512@users.noreply.github.com>
2023-12-20 17:26:03 +0900

    Add files via upload

---
+++ 2023-12-19[선택과제][2.메쏘드인자반환타입]/송선경/MemberFieldMain.java
@@ -0,0 +1,37 @@
+package com.itwill99.review;
+
+public class MemberFieldMain {
+
+	public static void main(String[] args) {
+		/*
+		 * 1.MemberField객체생성
+		 */
+		
+			MemberField mf = new MemberField();
+		
+		
+		/*
+		 * 2.MemberField객체의 멤버변수4개에 데이타대입
+		 */
+		
+			int mf1=10;
+			int mf2=20;
+			int mf3=30;
+			int mf4=40;
+			
+		
+		/*
+		 * 3.MemberField객체의 4개멤버변수내용출력
+		 */
+	
+			System.out.println("1번 멤버변수의 값: "+mf1);
+			System.out.println("2번 멤버변수의 값: "+mf2);
+			System.out.println("3번 멤버변수의 값: "+mf3);
+			System.out.println("4번 멤버변수의 값: "+mf4);
+	
+	
+	
+	
+	}
+
+}
---
+++ 2023-12-19[선택과제][2.메쏘드인자반환타입]/송선경/MemberMethodMain.java
@@ -0,0 +1,37 @@
+package com.itwill99.review;
+
+public class MemberMethodMain {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * MemberMethod객체생성
+		 */
+			MemberMethod mb = new MemberMethod();
+		/*
+		 * MemberMethodr객체 method1 호출
+		 */
+			mb.method1();
+		/*
+		 * MemberMethodr객체 method2 호출
+		 */
+			mb.method2();
+		/*
+		 * MemberMethod객체 method3 호출
+		 */
+			mb.method3(3);
+		/*
+		 * MemberMethodr객체 method4 호출
+		 */
+			mb.method4("Jenny",5);
+		/*
+		 * MemberMethodr객체 method5 호출
+		 */
+			mb.method5("Kate","Tony",7);
+		
+		
+		
+		
+	}
+
+}
