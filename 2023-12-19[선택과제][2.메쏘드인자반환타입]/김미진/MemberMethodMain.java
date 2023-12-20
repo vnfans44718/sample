eb55e5e8298b6efdb06cd0ebe3e540d1e30839a3
김미진 <149574990+slwla@users.noreply.github.com>
2023-12-20 18:08:17 +0900

    Add files via upload

---
+++ 2023-12-19[선택과제][2.메쏘드인자반환타입]/김미진/MemberMethodMain.java
@@ -0,0 +1,38 @@
+package com.itwill99.review;
+
+public class MemberMethodMain {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * MemberMethod객체생성
+		 */
+		MemberMethod mm = new MemberMethod();
+		/*
+		 * MemberMethodr객체 method1 호출
+		 */
+		mm.method1();
+		/*
+		 * MemberMethodr객체 method2 호출
+		 */
+		mm.method2();
+		/*
+		 * MemberMethod객체 method3 호출
+		 */
+		mm.method3(17);
+		/*
+		 * MemberMethodr객체 method4 호출
+		 */
+		mm.method4("낭랑", 18);
+		/*
+		 * MemberMethodr객체 method5 호출
+		 */
+		mm.method5("바다는 크레파스보다 진한,","푸르고 육중한 비늘을 무겁게 뒤채면서 숨을 쉰다", 20231220);
+		
+		
+		
+		
+		
+	}
+
+}
