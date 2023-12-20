e04302f71deda5bb8ecf0e48a29285398e83f01b
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-20 09:10:03 +0900

    Add files via upload

--- 2023-12-19[선택과제][2.메쏘드인자반환타입]/김하은/MemberFieldMain.java
+++ 2023-12-19[선택과제][2.메쏘드인자반환타입]/김하은/MemberFieldMain.java
@@ -4,17 +4,17 @@ public class MemberFieldMain {
 
 	public static void main(String[] args) {
 
-		MemberField homeLyrics = new MemberField();
+		MemberField johjiLyrics = new MemberField();
 
-		homeLyrics.a = "맞닿은 손가락이";
-		homeLyrics.b = "이제 더 이상 설레지 않아";
-		homeLyrics.c = "차가운 밤이 이렇게";
-		homeLyrics.d = "침대를 더럽히기 때문이야";
+		johjiLyrics.a = "맞닿은 손가락이";
+		johjiLyrics.b = "이제 더 이상 설레지 않아";
+		johjiLyrics.c = "차가운 밤이 이렇게";
+		johjiLyrics.d = "침대를 더럽히기 때문이야";
 		
-		System.out.println(homeLyrics.a);
-		System.out.println(homeLyrics.b);
-		System.out.println(homeLyrics.c);
-		System.out.println(homeLyrics.d);
+		System.out.println(johjiLyrics.a);
+		System.out.println(johjiLyrics.b);
+		System.out.println(johjiLyrics.c);
+		System.out.println(johjiLyrics.d);
 		
 	}
 }
