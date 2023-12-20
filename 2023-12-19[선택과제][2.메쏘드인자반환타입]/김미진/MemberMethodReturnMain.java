fcd8b65eb62325890bceff78f7889c19c3c32f93
김미진 <149574990+slwla@users.noreply.github.com>
2023-12-20 18:09:00 +0900

    Add files via upload

---
+++ 2023-12-19[선택과제][2.메쏘드인자반환타입]/김미진/MemberMethodReturnMain.java
@@ -0,0 +1,46 @@
+package com.itwill99.review;
+
+public class MemberMethodReturnMain {
+	public static void main(String[] args) {
+		/*
+		 * MemberMethodReturn 객체생성
+		 */
+		MemberMethodReturn mmr=
+				new MemberMethodReturn();
+		
+		/*
+		 MemberMethodReturn객체의  method1 호출후 반환데이타받아서 출력
+		 */
+		int returnData1 = mmr.method1();
+		System.out.println(returnData1);
+		/*
+		 MemberMethodReturn객체의  method2 호출후 반환데이타받아서 출력
+		 */
+		boolean returnData2 = mmr.method2();
+		System.out.println(returnData2);
+		/*
+		 MemberMethodReturn객체의 add메쏘드를 정수인자 두개를 넣어서 호출후 
+		 반환되는 정수데이타받아서 출력
+		 */
+		System.out.println(mmr.add(2485, 452));
+		/*
+		 MemberMethodReturn객체의 hello 메쏘드를 문자열인자 한개 넣어서 호출후 
+		 반환되는 문자열데이타받아서 출력
+		 */
+		System.out.println(mmr.hello("오리너구리"));
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
