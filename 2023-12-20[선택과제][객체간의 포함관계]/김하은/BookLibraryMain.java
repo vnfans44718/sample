e5bd42189c343153ca50433e8942ebe8ff041c93
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-21 10:03:45 +0900

    Add files via upload

--- 2023-12-20[선택과제][객체간의 포함관계]/김하은/BookLibraryMain.java
+++ 2023-12-20[선택과제][객체간의 포함관계]/김하은/BookLibraryMain.java
@@ -6,11 +6,10 @@ public class BookLibraryMain {
 		
 		BookMember member1 = new BookMember(3177, "김설탕", "01033852745", null);
 		
-		Book[] books = {
-				new Book(78, "더 검은 밤", "소설", "몽환과 영원의 세계"),
-				new Book(99, "무빙워크", "시", "정물이 되어본 적 있는 당신에게"),
-				new Book(13, "살구 칵테일", "철학", "사르트르와 하이데거의 이야기")
-				};
+		Book[] books = new Book[3];
+		books[0] = new Book(78, "더 검은 밤", "소설", "몽환과 영원의 세계");
+		books[1] = new Book(99, "무빙워크", "시", "정물이 되어본 적 있는 당신에게");
+		books[2] = new Book(13, "살구 칵테일", "철학", "사르트르와 하이데거의 이야기");
 		
 		member1.setBooks(books);
 		
