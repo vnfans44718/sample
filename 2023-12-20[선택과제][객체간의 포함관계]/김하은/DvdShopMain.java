c340e1eb65f9d6866cf5b5f866486261d651df2b
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-21 10:03:07 +0900

    Add files via upload

--- 2023-12-20[선택과제][객체간의 포함관계]/김하은/DvdShopMain.java
+++ 2023-12-20[선택과제][객체간의 포함관계]/김하은/DvdShopMain.java
@@ -6,12 +6,11 @@ public class DvdShopMain {
 		
 		DvdMember member1 = new DvdMember(109, "김초콜릿", "01012345678", null);
 		
-		Dvd[] rentDvds = {
-				new Dvd(87, "여학교의 별", "일상/개그"),
-				new Dvd(10, "주토피아", "애니메이션"),
-				new Dvd(34, "어바웃 타임", "멜로/로맨스"),
-				new Dvd(29, "헤어질 결심", "멜로/로맨스")
-				};
+		Dvd[] rentDvds = new Dvd[4];
+		rentDvds[0] = new Dvd(87, "여학교의 별", "일상/개그");
+		rentDvds[1] = new Dvd(10, "주토피아", "애니메이션");
+		rentDvds[2] = new Dvd(34, "어바웃 타임", "멜로/로맨스");
+		rentDvds[3] = new Dvd(29, "헤어질 결심", "멜로/로맨스");
 		
 		member1.setRentDvds(rentDvds);
 		
