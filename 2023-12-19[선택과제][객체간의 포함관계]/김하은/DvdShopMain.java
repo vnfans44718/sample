0e8917787f499691e560dd45f387309daa143f96
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-22 17:48:27 +0900

    Update DvdShopMain.java

--- 2023-12-19[선택과제][객체간의 포함관계]/김하은/DvdShopMain.java
+++ 2023-12-19[선택과제][객체간의 포함관계]/김하은/DvdShopMain.java
@@ -6,14 +6,13 @@ public class DvdShopMain {
 		
 		DvdMember member1 = new DvdMember(109, "김초콜릿", "01012345678", null);
 		DvdMember member2 = new DvdMember(285, "박마카롱", "01098765432", null);
-		DvdMember member3 = new DvdMember(952, "최캐러멜", "01073510186", null);
+		DvdMember member3 = new DvdMember(952, "최캐러멜", "01073510186");
 		
 		Dvd dvd1 = new Dvd(87, "여학교의 별", "일상/개그");
 		Dvd dvd2 = new Dvd(10, "주토피아", "애니메이션");
 		
 		member1.setRentDvd(dvd1);
 		member2.setRentDvd(dvd2);
-		member3.setRentDvd(null);
 		
 		DvdMember.headerPrint();
 		member1.memberPrint();
