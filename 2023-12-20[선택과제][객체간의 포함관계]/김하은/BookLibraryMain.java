6c38cbaf7761be1fdbef5a55d7ac0c46957216b7
김하은 <152938890+eunasoo@users.noreply.github.com>
2023-12-22 18:11:26 +0900

    Add files via upload

--- 2023-12-20[선택과제][객체간의 포함관계]/김하은/BookLibraryMain.java
+++ 2023-12-20[선택과제][객체간의 포함관계]/김하은/BookLibraryMain.java
@@ -4,17 +4,37 @@ public class BookLibraryMain {
 
 	public static void main(String[] args) {
 		
-		BookMember member1 = new BookMember(3177, "김설탕", "01033852745", null);
+		BookMember member1 = new BookMember(1089, "이시럽", "01075681256");
+		BookMember member2 = new BookMember(2462, "최토핑", "01026171088");
+		BookMember member3 = new BookMember(3177, "김설탕", "01033852745", null);
+		BookMember member4 = new BookMember(8901, "박연유", "01089175907", null);
 		
-		Book[] books = new Book[3];
-		books[0] = new Book(78, "더 검은 밤", "소설", "몽환과 영원의 세계");
-		books[1] = new Book(99, "무빙워크", "시", "정물이 되어본 적 있는 당신에게");
-		books[2] = new Book(13, "살구 칵테일", "철학", "사르트르와 하이데거의 이야기");
+		Book[] rentBooks1 = {
+				new Book(78, "더 검은 밤", "소설", "몽환과 영원의 세계"),
+				new Book(99, "무빙워크", "시", "정물이 되어본 적 있는 당신에게"),
+		};
 		
-		member1.setBooks(books);
+		Book[] rentBooks2 = {
+				new Book(13, "살구 칵테일", "철학", "사르트르와 하이데거의 이야기"),
+				new Book(57, "사랑 파먹기", "소설", "내가 네게 질 것 같으냐"),
+		};	
+		
+		member3.setBooks(rentBooks1);
+		member4.setBooks(rentBooks2);
 		
 		BookMember.headerPrint(); 
 		member1.memberPrint();
+		System.out.println();
+		BookMember.headerPrint();
+		member2.memberPrint();
+		System.out.println();
+		BookMember.headerPrint();
+		member3.memberPrint();
+		System.out.println();
+		BookMember.headerPrint();
+		member4.memberPrint();
+		System.out.println();
+		
 		
 	
 	}
