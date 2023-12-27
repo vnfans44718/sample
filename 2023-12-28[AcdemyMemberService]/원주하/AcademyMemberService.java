f45069629c08f0cadc1e94baaf26b4e418ae0446
원주하 <152963740+vnfans44718@users.noreply.github.com>
2023-12-27 20:53:22 +0900

    Add files via upload

---
+++ 2023-12-28[AcdemyMemberService]/원주하/AcademyMemberService.java
@@ -0,0 +1,301 @@
+package com.itwill05.service.array.academy;
+
+/*
+ * AcademyMember들의 업무처리클래스
+ *  Create
+ *  Read
+ *  Update
+ *  Delete
+ */
+public class AcademyMemberService {
+
+	private AcademyMember[] members;
+	private static final String SORT_BY_STUDENT = "Student";
+	private static final String SORT_BY_GANGSA = "Gangsa";
+	private static final String SORT_BY_STAFF = "Staff";
+
+	public AcademyMemberService() {
+		members = new AcademyMember[9];
+		members[0] = new AcademyStudent(1, "KIM", "자바");
+		members[1] = new AcademyStudent(2, "LEE", "리눅스");
+		members[2] = new AcademyStudent(3, "KIM", "자바");
+		members[3] = new AcademyStudent(4, "LEE", "IOT");
+		members[4] = new AcademyGangsa(5, "CHOI", "프로그래밍");
+		members[5] = new AcademyGangsa(6, "KIM", "DB");
+		members[6] = new AcademyGangsa(7, "DIM", "OS");
+		members[7] = new AcademyStaff(8, "AIM", "영업");
+		members[8] = new AcademyStaff(9, "QIM", "생산");
+
+	}
+
+	/*
+	 * 1.AcademyMember 전체출력
+	 */
+
+	public void academyMemberPrint() {
+		for (AcademyMember academyMember : members) {
+			academyMember.print();
+		}
+	}
+
+	/*
+	 * 2-1.번호 1 번 AcademyMember 객체 참조변수반환해줘
+	 * 
+	 */
+	public AcademyMember academyMemberReturn() {
+		AcademyMember tempMember = null;
+		for (int i = 0; i < members.length; i++) {
+			if (members[i].getNo() == 1) {
+				tempMember = members[i];
+			}
+		}
+
+		return tempMember;
+
+	}
+
+	/*
+	 * 2-2.이름 KIM 인 AcademyMember 배열객체 참조변수반환해줘
+	 */
+
+	public AcademyMember academyMemNameReturn(String name) {
+		AcademyMember tempMember = null;
+		for (int i = 0; i < members.length; i++) {
+			if (members[i].getName().equals(name)) {
+				tempMember = members[i];
+				break;
+			}
+		}
+
+		return tempMember;
+
+	}
+
+	/*
+	 * 3.AcademyMember 중에서 AcademyStudent객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]
+	 * 3.AcademyMember 중에서 AcademyStaff객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]
+	 * 3.AcademyMember 중에서 AcademyGangsa객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]
+	 */
+
+	public AcademyMember[] returnByMembers(String membertype) {
+		AcademyMember[] academyMember = null;
+		if (membertype.equals(SORT_BY_STUDENT)) {
+			academyMember = academyStudentReturn();
+		}
+
+		else if (membertype.equals(SORT_BY_GANGSA)) {
+			academyMember = academyGangsaReturn();
+		}
+
+		else if (membertype.equals(SORT_BY_STAFF)) {
+			academyMember = academyStaffReturn();
+		}
+
+		return academyMember;
+
+	}
+
+	private AcademyMember[] academyStudentReturn() {
+		int count = 0;
+		AcademyMember[] academyStudent = null;
+		for (AcademyMember academyMember : members) {
+			if (academyMember instanceof AcademyStudent) {
+				count++;
+			}
+
+		}
+		academyStudent = new AcademyMember[count];
+		int findcount = 0;
+
+		for (AcademyMember academyMember : members) {
+			if (academyMember instanceof AcademyStudent) {
+				academyStudent[findcount] = (AcademyStudent) academyMember;
+				findcount++;
+			}
+
+		}
+		return academyStudent;
+	}
+
+	private AcademyMember[] academyGangsaReturn() {
+		int count = 0;
+		AcademyMember[] academyGangsa = null;
+		for (AcademyMember academyMember : members) {
+			if (academyMember instanceof AcademyGangsa) {
+				count++;
+			}
+
+		}
+		academyGangsa = new AcademyMember[count];
+		int findcount = 0;
+
+		for (AcademyMember academyMember : members) {
+			if (academyMember instanceof AcademyGangsa) {
+				academyGangsa[findcount] = (AcademyGangsa) academyMember;
+				findcount++;
+			}
+
+		}
+		return academyGangsa;
+	}
+
+	public AcademyMember[] academyStaffReturn() {
+		int count = 0;
+		AcademyMember[] academyStaff = null;
+		for (AcademyMember academyMember : members) {
+			if (academyMember instanceof AcademyStaff) {
+				count++;
+			}
+
+		}
+		academyStaff = new AcademyMember[count];
+		int findcount = 0;
+
+		for (AcademyMember academyMember : members) {
+			if (academyMember instanceof AcademyStaff) {
+				academyStaff[findcount] = (AcademyStaff) academyMember;
+				findcount++;
+			}
+
+		}
+		return academyStaff;
+	}
+
+	/*
+	 * 4.AcademyMember 중에서 자바반인   AcademyStudent 객체들 배열참조변수반환해줘 
+	 * 4.AcademyMember 중에서 영업부서인 AcademyStaff   객체들 배열참조변수반환해줘 
+	 * 4.AcademyMember 중에서 자바과목인 AcademyGangsa  객체들 배열참조변수반환해줘
+	 */
+
+	public AcademyMember[] membersByBelong(String membertype, String belong) {
+		AcademyMember[] academyMember = null;
+		if (membertype.equals(SORT_BY_STUDENT)) {
+			academyMember = academyBanStudent(belong);
+		}
+
+		else if (membertype.equals(SORT_BY_GANGSA)) {
+			academyMember = academySubjectGangsa(belong);
+		}
+
+		else if (membertype.equals(SORT_BY_STAFF)) {
+			academyMember = academyDepartStaff(belong);
+		}
+
+		return academyMember;
+
+	}
+
+	private AcademyMember[] academyBanStudent(String belong) {
+		int count = 0;
+		AcademyMember[] academyStudent = null;
+		AcademyStudent tempacademyMember = null;
+		int findcount = 0;
+		for (AcademyMember academyMember : members) {
+
+			if (academyMember instanceof AcademyStudent) {
+				tempacademyMember = (AcademyStudent) academyMember;
+				if (tempacademyMember.getBan().equals(belong)) {
+					count++;
+				}
+			}
+
+			
+
+		}
+
+		academyStudent = new AcademyMember[count];
+
+		for (AcademyMember academyMember : members) {
+
+			if (academyMember instanceof AcademyStudent) {
+				tempacademyMember = (AcademyStudent) academyMember;
+
+			}
+
+			if (tempacademyMember.getBan().equals(belong)) {
+				academyStudent[findcount] = tempacademyMember;
+				findcount++;
+			}
+
+		}
+
+		return academyStudent;
+	}
+
+	private AcademyMember[] academyDepartStaff(String belong) {
+		int count = 0;
+		AcademyMember[] academyStaff = null;
+		AcademyStaff tempacademyMember = null;
+		int findcount = 0;
+		for (AcademyMember academyMember : members) {
+
+			if (academyMember instanceof AcademyStaff) {
+				tempacademyMember = (AcademyStaff) academyMember;
+				if (tempacademyMember.getDepart().equals(belong)) {
+					count++;
+				}
+			}
+
+		}
+
+		academyStaff = new AcademyMember[count];
+
+		for (AcademyMember academyMember : members) {
+
+			if (academyMember instanceof AcademyStaff) {
+				tempacademyMember = (AcademyStaff) academyMember;
+
+			}
+
+			if (tempacademyMember.getDepart().equals(belong)) {
+				academyStaff[findcount] = tempacademyMember;
+				findcount++;
+			}
+
+		}
+
+		return academyStaff;
+	}
+
+	private AcademyMember[] academySubjectGangsa(String belong) {
+		int count = 0;
+		AcademyMember[] academyGangsa = null;
+		AcademyGangsa tempacademyMember = null;
+		int findcount = 0;
+		for (AcademyMember academyMember : members) {
+
+			if (academyMember instanceof AcademyGangsa) {
+				tempacademyMember = (AcademyGangsa) academyMember;
+				if (tempacademyMember.getSubject().equals(belong)) {
+					count++;
+				}
+			}
+
+		}
+
+		academyGangsa = new AcademyMember[count];
+
+		for (AcademyMember academyMember : members) {
+
+			if (academyMember instanceof AcademyGangsa) {
+				tempacademyMember = (AcademyGangsa) academyMember;
+
+			}
+
+			if (tempacademyMember.getSubject().equals(belong)) {
+				academyGangsa[findcount] = tempacademyMember;
+				findcount++;
+			}
+
+		}
+
+		return academyGangsa;
+	}
+
+	/*
+	 * 5.AcademyMember 중에서 자바반인   AcademyStudent 객체들 배열참조변수반환해줘[OPTION]
+	 * 5.AcademyMember 중에서 영업부서인 AcademyStaff   객체들 배열참조변수반환해줘[OPTION] 
+	 * 5.AcademyMember 중에서 자바과목인 AcademyGangsa  객체들 배열참조변수반환해줘[OPTION]
+	 */
+
+}
---
+++ 2023-12-28[AcdemyMemberService]/원주하/AcademyMemberServiceMain.java
@@ -0,0 +1,58 @@
+package com.itwill05.service.array.academy;
+
+public class AcademyMemberServiceMain {
+
+	public static void main(String[] args) {
+		System.out.println("1.AcademyMember 전체출력");
+		AcademyMemberService academymembers = new AcademyMemberService();
+		academymembers.academyMemberPrint();
+
+		System.out.println("2-1.번호 1 번 AcademyMember 객체 참조변수반환해줘");
+		AcademyMember memberReturn = academymembers.academyMemberReturn();
+		memberReturn.print();
+		System.out.println("2-2.이름 KIM 인 AcademyMember 배열객체 참조변수반환해줘");
+		AcademyMember memberNameReturn = academymembers.academyMemNameReturn("KIM");
+		memberNameReturn.print();
+		System.out.println("3.AcademyMember 중에서 AcademyStudent객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]");
+		AcademyMember[] academyStudent = academymembers.returnByMembers("Student");
+		for (int i = 0; i < academyStudent.length; i++) {
+			academyStudent[i].print();
+		}
+		System.out.println("3.AcademyMember 중에서 AcademyGangsa객체들 모두반환해줘 [ AcademyMember[] 객체주소반환 ]");
+		AcademyMember[] academyGangsa = academymembers.returnByMembers("Gangsa");
+		for (int i = 0; i < academyGangsa.length; i++) {
+			academyGangsa[i].print();
+		}
+		System.out.println("3.AcademyMember 중에서 AcademyStaff객체들  모두반환해줘 [ AcademyMember[] 객체주소반환 ]");
+		AcademyMember[] academyStaff = academymembers.returnByMembers("Staff");
+		for (int i = 0; i < academyStaff.length; i++) {
+			academyStaff[i].print();
+		}
+
+		System.out.println("4.AcademyMember 중에서 자바반인  AcademyStudent객체들 배열참조변수반환해줘");
+
+		AcademyMember[] academyJavaStudent = academymembers.membersByBelong("Student", "자바");
+		for (int i = 0; i < academyJavaStudent.length; i++) {
+
+			academyJavaStudent[i].print();
+		}
+
+		System.out.println("4.AcademyMember 중에서 영업부서인 AcademyStaff  객체들 배열참조변수반환해줘");
+
+		AcademyMember[] SalesStaff = academymembers.membersByBelong("Staff", "영업");
+		for (int i = 0; i < SalesStaff.length; i++) {
+			SalesStaff[i].print();
+		}
+		System.out.println("4.AcademyMember 중에서 자바과목인 AcademyGangsa 객체들 배열참조변수반환해줘");
+		AcademyMember[] academyJavaGangsa = academymembers.membersByBelong("Gangsa", "자바");
+		for (int i = 0; i < academyJavaGangsa.length; i++) {
+			academyJavaGangsa[i].print();
+		}
+		System.out.println("---------------------------------------------");
+		System.out.println("5.AcademyMember 중에서 자바반인  AcademyStudent객체들 배열참조변수반환해줘");
+		System.out.println("5.AcademyMember 중에서 영업부서인 AcademyStaff  객체들 배열참조변수반환해줘");
+		System.out.println("5.AcademyMember 중에서 자바과목인 AcademyGangsa 객체들 배열참조변수반환해줘");
+
+	}
+
+}
