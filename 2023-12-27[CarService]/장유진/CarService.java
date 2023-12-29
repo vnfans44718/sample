c93643071cbd52ece7089f4f70da6d05bc48a2fa
장유진 <152938410+r-u-5k@users.noreply.github.com>
2023-12-29 13:28:45 +0900

    Update CarService.java

--- 2023-12-27[CarService]/장유진/CarService.java
+++ 2023-12-27[CarService]/장유진/CarService.java
@@ -45,9 +45,9 @@ public class CarService {
 		}
 		
 		// 3. 입차
-		for (int i = 0; i < carArray.length; i++) {
-			if (carArray[i] == null) {
-				carArray[i] = car;
+		for (Car car2 : carArray) {
+			if (car2 == null) {
+				car2 = car;
 				break;
 			}
 		}
