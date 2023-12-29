517ab2cc6472995853897c33bc1cec29371a233f
academyitwill <academyitwill@gmail.com>
2023-12-29 14:29:05 +0900

    ..

--- 2023-12-29[AccountService-ArrayList]/최유강/LackOfFundsException.java
+++
@@ -1,9 +0,0 @@
-package com.itwill05.service.collection.account;
-
-public class LackOfFundsException extends Exception {
-
-	public LackOfFundsException(String msg) {
-		// TODO Auto-generated constructor stub
-		super(msg);
-	}
-}
--- 2023-12-29[AccountService-ArrayList]/최유강/MemberNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.itwill05.service.collection.account;
-
-public class MemberNotFoundException extends Exception {
-public MemberNotFoundException(String msg) {
-	// TODO Auto-generated constructor stub
-	super(msg);
-}
-}
